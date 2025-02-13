bae2a5d160a88b94983c8fcb659791b8c9bbe7b3
Roshan <scryomancer@gmail.com>
2025-02-13 15:40:52 +0800

    Final Changes in Data Input.py

--- src/data_input.py
+++ src/data_input.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import requests
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 # Render an HTML form for file upload
 @app.route('/')
@@ -72,5 +72,5 @@ def upload_file():
         return jsonify({"error": "Failed to connect to backend", "details": str(e)}), 500
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True,port=5000)
